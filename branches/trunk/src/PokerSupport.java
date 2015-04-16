862701eb3e938c6610bf58b1545dee95705e1766
AdamBoczula <adam.boczula@hotmail.com>
2015-04-16 06:47:40 +0200

    Porzadek, pracujemy na 1 branchu - na trunku. Zmodyfikowany projekt; sciezka do sqlite

--- branches/trunk/src/PokerSupport.java
+++ branches/trunk/src/PokerSupport.java
@@ -2,29 +2,28 @@ import java.nio.file.Paths;
 
 import javafx.scene.shape.Path;
 
-
-
 public class PokerSupport {
-	private PokerDataBase dataBase;
-	private FileReader fileReader;
-	
-	public PokerDataBase getDataBase() {
-		return PokerDataBase.getInstance();
-	}
-	
-	public FileReader getFileReader() {
-		return FileReader.getInstance();
-	}
-	
-	public PokerSupport() {
-		dataBase = PokerDataBase.getInstance();
-		fileReader = FileReader.getInstance();
-	}
+    private PokerDataBase dataBase;
+    private FileReader fileReader;
 
+    public PokerDataBase getDataBase( ) {
+        return PokerDataBase.getInstance( );
+    }
 
-	public static void main(String[] args) {
-		PokerSupport poker = new PokerSupport();
-		poker.getFileReader().setPathToFolder(Paths.get("C:\\Users\\adam\\Documents\\HandHistory\\Bcl333"));
-		poker.getFileReader().findSummmaryFiles();
-	}
+    public FileReader getFileReader( ) {
+        return FileReader.getInstance( );
+    }
+
+    public PokerSupport( ) {
+        dataBase = PokerDataBase.getInstance( );
+        fileReader = FileReader.getInstance( );
+    }
+
+    public static void main(String[ ] args) {
+        System.out.println("dupa debaging");
+        PokerSupport poker = new PokerSupport( );
+        poker.getFileReader( ).setPathToFolder(Paths.get("C:\\Users\\adam\\Documents\\HandHistory\\Bcl333"));
+        poker.getFileReader( ).findSummmaryFiles( );
+
+    }
 }
